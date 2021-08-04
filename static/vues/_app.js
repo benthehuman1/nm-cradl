64d15c610a67dbfa2abc098371e9dccf383b0cf2
ANISH MALIK <anishmalik@northwesternmutual.com>
2021-08-04 15:14:02 -0500

    added navbar and logo

--- static/vues/_app.js
+++ static/vues/_app.js
@@ -1,18 +1,13 @@
 const appComponentTemplate = `
-
     <div id="appComponent">
-        <section class="hero is-info welcome is-small" style="border-radius: 6px 6px 0px 0px;">
-            <div class="hero-body">
-                <div class="container">
-                    <h1 class="title">
-                        Welcome to Cradl
-                    </h1>
-                    <h2 class="subtitle">
-                        Compare different plans!
-                    </h2>
-                </div>
-            </div>
-        </section>
+        <div>
+        <img src="/static/assets/cradl_logo.png" alt="Cradl logo" style="width: 161px; height: 65px;">
+    </div>
+    <div class="topnav">
+        <a class="active" href="https://laurenbrown1886.wixsite.com/website">Home</a>
+        <a href="/">Explore Plans</a>
+        <a href="/compare">Compare Plans</a>
+    </div>
         <div class="columns">
             <div class="column is-6" style="width: 50%">
                 <chart/>
--- static/vues/app.js
+++ static/vues/app.js
@@ -1,17 +1,13 @@
 const appComponentTemplate = `
 <div>
-    <section class="hero is-info welcome is-small" style="border-radius: 6px 6px 0px 0px;">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    Welcome to Cradl
-                </h1>
-                <h2 class="subtitle">
-                    See the magic of compounding interest!
-                </h2>
-            </div>
-        </div>
-    </section>
+    <div>
+        <img src="/static/assets/cradl_logo.png" alt="Cradl logo" style="width: 161px; height: 65px;">
+    </div>
+    <div class="topnav">
+        <a class="active" href="https://laurenbrown1886.wixsite.com/website">Home</a>
+        <a href="/">Explore Plans</a>
+        <a href="/compare">Compare Plans</a>
+    </div>
     <chart/>
 </div>
 `;
